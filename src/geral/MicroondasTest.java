8c592fab3c5ef6c93e340d152a087216d7ebf9d5
LucasLottiPzm <lucas.lotti@grupopzm.com>
2022-03-09 00:08:57 -0300

    criado arquivo de teste e ativando biblioteca junit

---
+++ src/geral/MicroondasTest.java
@@ -0,0 +1,8 @@
+package geral;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MicroondasTest {
+}
