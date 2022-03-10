88ac8144ee05136913205bc31aa3c17e82f621f3
LucasLottiPzm <lucas.lotti@grupopzm.com>
2022-03-10 20:32:01 -0300

    adding functions of add seconds, print, constructor, open door and doing their test.

--- src/geral/Microondas.java
+++ src/geral/Microondas.java
@@ -2,9 +2,85 @@ package geral;
 
 public class Microondas {
     
-    //atributos 
-    private boolean situacaoPorta;
-    private int estado;
+    //atributos
+    public boolean situacaoPorta;
+    public boolean estado;
     public int segundos;
     public int minutos;
+
+    private void init(int minutos, int segundos){
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    /**
+     * Construtor colocando o parâmetro minutos e colocando o parâmetro segundos.
+     * @param minutos
+     * @param segundos
+     */
+    public Microondas(int minutos, int segundos){
+        this.init(minutos, segundos);
+    }
+
+    /**
+     * Construtor colocando apenas o parâmetro segundos
+     * @param segundos
+     */
+    public Microondas(int segundos){
+        this.init(0 ,segundos);
+    }
+
+    /**
+     * Função que retorna o tempo de minuto e tempo.
+     * @return
+     */
+    public String mostraTempo(){
+        String minutosFinal = String.format("%02d", this.minutos);
+        String segundosFinal = String.format("%02d", this.segundos);
+        return minutosFinal + ':' + segundosFinal;
+    }
+
+    /**
+     * Função que adiciona segundos 
+     * @param segundosAdicionados
+     * @return void logo ela muda valores de segundo e minuto e não retorna nada.
+     */
+    public void adicionarSegundos(int segundosAdicionados){
+        for(int i = 0; i < segundosAdicionados; i++){
+            if(dataValida() != 0){
+                if(dataValida() == 1){
+                    this.segundos = 0;
+                    this.minutos ++;
+                }else{
+                    break;
+                }
+            }else{
+                this.segundos++;   
+            }
+        }
+    }
+    /**
+     * Função que abre porta
+     * @return situacaoPorta bool que fornece se a porta está fechada ou aberta.
+     */
+    public Boolean abrirPorta(){
+        if(estado == false){
+            situacaoPorta = true;
+        }
+        return situacaoPorta;
+    }
+
+    /**
+     * Estipulado o máximo para ser 60 minutos, retorna de 1 segundo até 60 minutos.
+     * @return extrapolou int que fornece se extrapolou minuto ou segundo.
+     */
+    private int dataValida(){
+        int extrapolou = 0;
+        if(this.segundos >= 59){
+            extrapolou = 1;
+        }else if(this.minutos == 60){
+            extrapolou = 2;
+        }
+        return extrapolou;
+    }
 }
--- src/geral/MicroondasTest.java
+++ src/geral/MicroondasTest.java
@@ -5,4 +5,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MicroondasTest {
+    
+    Microondas tempo;
+    Microondas soSegundos;
+
+    @BeforeEach
+    void init(){
+        tempo = new Microondas(1, 10);
+        soSegundos = new Microondas(50);
+    }
+
+    @Test
+    void testarTempo(){
+        assertEquals("01:10", tempo.mostraTempo());
+    }
+
+    @Test
+    void testarTempoSegundos(){
+        assertEquals("00:50", soSegundos.mostraTempo());
+    }
+
+    @Test
+    void adicionarTempo(){
+        tempo.adicionarSegundos(10);
+        assertEquals("01:20", tempo.mostraTempo());
+    }
+
+    @Test
+    void adicionarTempoAMais(){
+        soSegundos.adicionarSegundos(11);
+        assertEquals("01:01", soSegundos.mostraTempo());
+    }
+
+    @Test
+    void adicionarTempoUltrapassandoLimite(){
+        soSegundos.adicionarSegundos(6000);
+        assertEquals("60:00", soSegundos.mostraTempo());
+    }
+
+    @Test
+    void situacaoPorta(){
+        soSegundos.abrirPorta();
+        assertEquals(false, soSegundos.situacaoPorta);
+    }
+
+
 }
